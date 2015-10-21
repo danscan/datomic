acbb73775d99839c4144fd7ca5ca5d650a59b29d
Dan Scanlon <dscanlonpa@gmail.com>
2015-10-20 21:14:01 -0400

    Removed magic string

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ const DEFAULT_PROTOCOL = 'http';
 const DEFAULT_HOST = 'localhost';
 const DEFAULT_PORT = 8080;
 const DEFAULT_STORAGE_ALIAS = 'dev';
+const DEFAULT_BASIS_TIME = '-'; // Datomic current-time basis
 
 const resources = {
   storageAliases: () => `/data/`,
   databases: ({ storageAlias }) => `/data/${storageAlias}/`,
   database: ({ storageAlias, database }) => `${resources.databases({ storageAlias })}${database}/`,
-  databaseData: ({ storageAlias, database, basisTime = '-' }) => `${resources.database({ storageAlias, database })}/${basisTime}/`,
+  databaseData: ({ storageAlias, database, basisTime = DEFAULT_BASIS_TIME }) => `${resources.database({ storageAlias, database })}/${basisTime}/`,
   datoms: ({ storageAlias, database, basisTime }) => `${resources.databaseData({ storageAlias, database, basisTime })}datoms`,
   events: ({ storageAlias, database }) => `/events/${storageAlias}/${database}`,
 };
@@ -20,9 +21,9 @@ export default ({ protocol = DEFAULT_PROTOCOL, host = DEFAULT_HOST, port = DEFAU
     listDatabases: () => rootUri + resources.databases({ storageAlias }),
     createDatabase: () => rootUri + resources.databases({ storageAlias }),
     transact: () => rootUri + resources.database({ storageAlias, database }),
-    getDatabaseInfo: ({ basisTime = '-' }) => rootUri + resources.databaseData({ storageAlias, database, basisTime }),
-    getDatoms: ({ basisTime = '-' }) => `${rootUri}${resources.databaseData({ storageAlias, database, basisTime })}datoms`,
-    getEntity: ({ basisTime = '-' }) => `${rootUri}${resources.databaseData({ storageAlias, database, basisTime })}entity`,
+    getDatabaseInfo: ({ basisTime = DEFAULT_BASIS_TIME }) => rootUri + resources.databaseData({ storageAlias, database, basisTime }),
+    getDatoms: ({ basisTime = DEFAULT_BASIS_TIME }) => `${rootUri}${resources.databaseData({ storageAlias, database, basisTime })}datoms`,
+    getEntity: ({ basisTime = DEFAULT_BASIS_TIME }) => `${rootUri}${resources.databaseData({ storageAlias, database, basisTime })}entity`,
     query: () => `${rootUri}/api/query`,
     subscribeToEvents: () => rootUri + resources.events({ storageAlias, database }),
   };
