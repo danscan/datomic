d0b8ed71abc763f0dadc8eeef4dc4a0d8be7476c
Dan Scanlon <dscanlonpa@gmail.com>
2015-10-20 20:22:00 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,168 @@
+import routes from './routes';
+import request from 'superagent';
+import edn from 'jsedn';
+
+// Configuration constants
+const EDN_MIME_TYPE = 'application/edn';
+
+export default class Datomic {
+  constructor({ host, port, storageAlias, database, protocol }) {
+    this.routes = routes({ host, port, storageAlias, database, protocol });
+
+    Datomic.listDatabases({ host, port, storageAlias, protocol })
+      .then(databases => {
+        const databaseExists = databases.indexOf(database) >= 0;
+
+        if (!databaseExists) {
+          console.log(`Database '${database}' does not exist...`);
+          return Datomic.createDatabase({ host, port, storageAlias, database, protocol });
+        }
+
+        console.log(`Database '${database} exists...`);
+      });
+  }
+
+  static createDatabase({ host, port, storageAlias, database, protocol }) {
+    const { createDatabase: getRoute } = routes({ host, port, storageAlias, database, protocol });
+
+    return requestEdn({
+      uri: getRoute(),
+      method: 'post',
+      body: new edn.Map([edn.kw(':db-name'), database]),
+    });
+  }
+
+  static listAliases({ host, port, protocol } = {}) {
+    const { listAliases: getRoute } = routes({ host, port, protocol });
+
+    return requestEdn({
+      uri: getRoute(),
+      method: 'get',
+    });
+  }
+
+  static listDatabases({ host, port, storageAlias, protocol } = {}) {
+    const { listDatabases: getRoute } = routes({ host, port, storageAlias, protocol });
+
+    return requestEdn({
+      uri: getRoute(),
+      method: 'get',
+    });
+  }
+
+  transact(transactionData) {
+    return requestEdn({
+      uri: this.routes.transact(),
+      method: 'post',
+      body: new edn.Map([edn.kw(':tx-data'), transactionData]),
+    });
+  }
+
+  getDatabaseInfo({ basisT }) {
+    return requestEdn({
+      uri: this.routes.getDatabaseInfo({ basisT }),
+      method: 'get',
+    });
+  }
+
+  getDatoms({ basisT, index, entity, attribute, value, startValue, endValue, limit, offset, asOfT, sinceT, historical }) {
+    const options = {
+      index,
+      'e': entity,
+      'a': attribute,
+      'v': value,
+      'start': startValue,
+      'end': endValue,
+      limit,
+      offset,
+      'as-of-t': asOfT,
+      'since-t': sinceT,
+      'history': historical,
+    };
+
+    return requestEdn({
+      uri: this.routes.getDatoms({ basisT }),
+      method: 'get',
+      query: options,
+    });
+  }
+
+  getEntity({ basisT, entity, asOfT, sinceT }) {
+    const options = {
+      'e': entity,
+      'as-of-t': asOfT,
+      'since-t': sinceT,
+    };
+
+    return requestEdn({
+      uri: this.routes.getEntity({ basisT }),
+      method: 'get',
+      query: options,
+    });
+  }
+
+  query({ query, args }) {
+    // TODO: Validate query..?
+    console.log('TODO: Validate query..?');
+
+    return requestEdn({
+      uri: this.routes.query(),
+      method: 'post',
+      body: new edn.Map([edn.kw(':q'), query, edn.kw(':args'), args]),
+    });
+  }
+
+  // TODO: Implement subscribeToEvents...
+  subscribeToEvents(/* { basisT } */) {
+    return Promise.reject('Not yet implemented...');
+
+    // return requestEdn({
+    //   uri: this.routes.subscribeToEvents({ basisT }),
+    //   method: 'get',
+    // });
+  }
+}
+
+// Private helpers
+function requestEdn({ uri, method, query, body }) {
+  // Determine request method name from method param
+  let methodName = method.toLowerCase();
+  methodName = methodName === 'delete'
+              ? 'del'
+              : methodName;
+
+  // Encode body into edn
+  const ednBody = !!body
+                ? edn.encode(body)
+                : undefined;
+
+  console.log('requestEdn...');
+  console.log('methodName:', methodName);
+  console.log('uri:', uri);
+  console.log('query:', query);
+  console.log('body:', body);
+  console.log('ednBody:', ednBody);
+
+  return new Promise((resolve, reject) => {
+    request[methodName](uri)
+      .type(EDN_MIME_TYPE)
+      .accept(EDN_MIME_TYPE)
+      .buffer(true)
+      .query(query)
+      .send(ednBody)
+      .end((error, res) => {
+        if (error) {
+          console.error('requestEdn error:', error);
+          return reject(error);
+        }
+
+        const ednResponseBody = res.text;
+        console.log('requestEdn... ednResponseBody:', ednResponseBody);
+
+        const jsonResponseBody = edn.parse(ednResponseBody);
+        console.log('requestEdn... jsonResponseBody:', jsonResponseBody);
+
+        return resolve(jsonResponseBody.val);
+      });
+  });
+}
---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+const DEFAULT_PROTOCOL = 'http';
+const DEFAULT_HOST = 'localhost';
+const DEFAULT_PORT = 8080;
+const DEFAULT_STORAGE_ALIAS = 'dev';
+
+const resources = {
+  storageAliases: () => `/data/`,
+  databases: ({ storageAlias }) => `/data/${storageAlias}/`,
+  database: ({ storageAlias, database }) => `${resources.databases({ storageAlias })}${database}/`,
+  databaseData: ({ storageAlias, database, basisT = '-' }) => `${resources.database({ storageAlias, database })}/${basisT}/`,
+  datoms: ({ storageAlias, database, basisT }) => `${resources.databaseData({ storageAlias, database, basisT })}datoms`,
+  events: ({ storageAlias, database }) => `/events/${storageAlias}/${database}`,
+};
+
+export default ({ protocol = DEFAULT_PROTOCOL, host = DEFAULT_HOST, port = DEFAULT_PORT, storageAlias = DEFAULT_STORAGE_ALIAS, database }) => {
+  const rootUri = `${protocol}://${host}:${port}`;
+
+  return {
+    listAliases: () => rootUri + resources.storageAliases(),
+    listDatabases: () => rootUri + resources.databases({ storageAlias }),
+    createDatabase: () => rootUri + resources.databases({ storageAlias }),
+    transact: () => rootUri + resources.database({ storageAlias, database }),
+    getDatabaseInfo: ({ basisT = '-' }) => rootUri + resources.databaseData({ storageAlias, database, basisT }),
+    getDatoms: ({ basisT = '-' }) => `${rootUri}${resources.databaseData({ storageAlias, database, basisT })}datoms`,
+    getEntity: ({ basisT = '-' }) => `${rootUri}${resources.databaseData({ storageAlias, database, basisT })}entity`,
+    query: () => `${rootUri}/api/query`,
+    subscribeToEvents: () => rootUri + resources.events({ storageAlias, database }),
+  };
+};
